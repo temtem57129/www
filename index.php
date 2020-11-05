c76ba89a89a7191143086efa835e601c30a26b98
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-05 20:51:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <body>
+    <h1>MY FIRST WEB</h1>
+    <p>นาย กันตวิชญ์ จองกฤษ ชื่อเล่น "เต็ม" </p>
+    <h1>My Favorite Animal</h1>
+    <img src="cat1.jpg" width="500" height="300"/>
+    <p></p>
+    <img src="cat2.jpg" width="500" height="300"/>
+    <br />
+    <a href="https://genshin.mihoyo.com/en">This is a Lick</a>
+    <h1>เล่าเรื่องตลก</h1>
+    <p>Aถาม : รู้ไหมอะไรเป็นตัวการทำให้โลกร้อน</p>
+    <p>Bตอบ : เพราะว่าคนตัดต้นไม้เยอะโลกเลยร้อน คนเป็นตัวการ</p>
+    <p>Aเฉลย : ผิด! หมีต่างหากละที่เป็นตัวการเพราะว่าหมีขั้วโลก โลกเลยร้อน</p>
+  </body>
+</html>
