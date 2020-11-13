2370f8743fe9bbd7228aeb541c7d6ff44fecae82
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-13 10:27:28 +0700

    Create show.php

---
+++ show.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>ITF Lab</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'kantawit11.mysql.database.azure.com', 'kantawit123@kantawit11', 'OGUlot28', 'ITFlab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">name</div></th>
+    <th width="350"> <div align="center">comment </div></th>
+    <th width="150"> <div align="center">link </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['name'];?></div></td>
+    <td><?php echo $Result['comment'];?></td>
+    <td><?php echo $Result['link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
