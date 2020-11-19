6d49eeeeea24cb157c878125aa70e1eb1de5bedf
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-19 10:38:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <body>
     <h1>MY FIRST WEB</h1>
     <p>Hello</p>
-    <img src="klee.jpg" />
+    <img src="cat1.jpg" />
+    <img src="cat2.jpg" />
     <br />
     <a href="https://genshin.mihoyo.com/en%22%3EThis is a Link</a>
     <h2>The list</h2>
@@ -14,5 +15,7 @@
       <li>Amazon</li>
       <li>Yahoo</li>
     </ul>
+    <p> รู้ไหมทำโลกถึงร้อน </p>
+    <p> เพราะหมีขั่วโลก </p>
   </body>
 </html>
