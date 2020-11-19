59a9036ea0e53efed69b2e8ccd1514dd92e23667
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-19 10:34:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<html>
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <h1>MY FIRST WEB</h1>
+    <p>Hello</p>
+    <img src="klee.jpg" />
+    <br />
+    <a href="https://genshin.mihoyo.com/en%22%3EThis is a Link</a>
+    <h2>The list</h2>
+    <ul>
+      <li>Google</li>
+      <li>Amazon</li>
+      <li>Yahoo</li>
+    </ul>
+  </body>
+</html>
