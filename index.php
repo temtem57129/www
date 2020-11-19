9f5be091fa1424c53a3fa95dcd8a249ce90e729a
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-19 10:40:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
 <html>
-  <head>
-    <title>Hello</title>
-  </head>
   <body>
     <h1>MY FIRST WEB</h1>
-    <p>Hello</p>
-    <img src="cat1.jpg" />
-    <img src="cat2.jpg" />
+    <p>นาย กันตวิชญ์ จองกฤษ ชื่อเล่น "เต็ม" </p>
+    <h1>My Favorite Animal</h1>
+    <img src="cat1.jpg" width="500" height="300"/>
+    <p></p>
+    <img src="cat2.jpg" width="500" height="300"/>
     <br />
-    <a href="https://genshin.mihoyo.com/en%22%3EThis is a Link</a>
-    <h2>The list</h2>
-    <ul>
-      <li>Google</li>
-      <li>Amazon</li>
-      <li>Yahoo</li>
-    </ul>
-    <p> รู้ไหมทำโลกถึงร้อน </p>
-    <p> เพราะหมีขั่วโลก </p>
+    <a href="https://genshin.mihoyo.com/en">This is a Lick</a>
+    <h1>เล่าเรื่องตลก</h1>
+    <p>Aถาม : รู้ไหมอะไรเป็นตัวการทำให้โลกร้อน</p>
+    <p>Bตอบ : เพราะว่าคนตัดต้นไม้เยอะโลกเลยร้อน คนเป็นตัวการ</p>
+    <p>Aเฉลย : ผิด! หมีต่างหากละที่เป็นตัวการเพราะว่าหมีขั้วโลก โลกเลยร้อน</p>
   </body>
 </html>
