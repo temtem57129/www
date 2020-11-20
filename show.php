e0792dbe74a3eac764866fa1835f52f8945be224
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-20 15:22:34 +0700

    Update show.php

--- show.php
+++ show.php
@@ -30,7 +30,8 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['link'];?></td>
     <td><?php echo '<a href="delete.php?id='.$Result['id'].'">
 Delete</a>'?></td>
-    <td><?php echo '<a href="insert_new.php?id='.$Result['id'].'"> Update</a>’?></td>
+    <td><?php echo '<a href="insert_new.php?id='.$Result['id'].'">
+Update</a>'?></td>
 
   </tr>
 <?php
