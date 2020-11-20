2b7f4838eae0f34a4a72ddcec9f7947116822759
temtem57129 <73679652+temtem57129@users.noreply.github.com>
2020-11-20 11:20:42 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,8 +5,8 @@ if (mysqli_connect_errno($conn))
 {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$id = $_GET['ID'];
-$sql = "DELETE FROM guestbook WHERE ID = $ID";
+$id = $_GET['id'];
+$sql = "DELETE FROM guestbook WHERE id = $id";
 if (mysqli_query($conn, $sql)) {
 echo "Record deleted successfully";
 } else {
